49e0f981c2906957f0db9ea1bb6c45ea141beab7
FINEP UTFPR <jeanbrandaocarvalho@hotmail.com>
2024-05-21 18:50:25 -0300

    New routes for test

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,19 @@
 import { FastifyInstance, FastifyPluginOptions, FastifyRequest, FastifyReply } from "fastify";
 import { ListFilesController } from "./controllers/ListFilesController";
+import { ListCarController } from "./controllers/ListCarController";
+import { ListGeoController } from "./controllers/ListGeoController";
 
 export async function routes(fastify: FastifyInstance, options: FastifyPluginOptions)
 {
     fastify.get("/videofiles", async (request: FastifyRequest, reply: FastifyReply) => {
         return new ListFilesController().handle(request, reply)
     })         
+
+    fastify.get("/measurements", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new ListCarController().handle(request, reply)
+    })
+
+    fastify.get("/geodata", async (request: FastifyRequest, reply: FastifyReply) => {
+        return new ListGeoController().handle(request, reply)
+    })
 }
